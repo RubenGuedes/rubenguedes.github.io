e437242b845f529e9ecd7506aeb0d96cfdf38ef0
Ruben Peixoto <rubpei2@hotmail.com>
2023-09-18 22:50:27 +0100

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript to show/hide scroll-to-top icon when footer is reached
+window.addEventListener("scroll", function () {
+    let scrollToTop = document.querySelector(".scroll-to-top");
+    let footer = document.querySelector("footer");
+
+    if (footer.getBoundingClientRect().top <= window.innerHeight && footer.getBoundingClientRect().top > 0) {
+        scrollToTop.style.display = "block";
+    }
+    else {
+        scrollToTop.style.display = "none";
+    }
+});
+
+// JavaScript code to scroll to the top of the page
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // This makes the scroll smooth
+    });
+}
